9b6713a7f6166342726e62e7a3008536cc49e370
Marek Dědič <developer@dedic.eu>
2019-02-25 00:25:31 +0100

    Making side panel image selector universal

--- src/ts/actions/addField.ts
+++ src/ts/actions/addField.ts
@@ -1,17 +1,17 @@
 /* exported addField */
 
-function openFieldImageSelector(name: string, description: string, image: string, page = 1 , perPage = 15): void
+function openSidePanelImageSelector(callback: (state: any) => void, state: any, page = 1 , perPage = 15): void
 {
 	sidePanelDoubleOpen();
 	document.getElementById("sidePanel")!.innerHTML = "<div id=\"embeddedSpinner\"></div>";
 	request(CONFIG.apiuri + "/image", "GET", {}, function(response: RequestResponse): void
 	{
-		renderFieldImageSelector(response as unknown as Array<string>, name, description, image, page, perPage); // eslint-disable-line @typescript-eslint/no-use-before-define
+		renderSidePanelImageSelector(response as unknown as Array<string>, callback, state, page, perPage); // eslint-disable-line @typescript-eslint/no-use-before-define
 	}, reAuthHandler);
 	refreshLogin();
 }
 
-function renderFieldImageSelector(list: Array<string>, name: string, description: string, image: string, page: number, perPage: number): void
+function renderSidePanelImageSelector(list: Array<string>, callback: (state: any) => void, state: any, page: number, perPage: number): void
 {
 	var html = "<div class=\"button yellowButton\" id=\"fieldImageCancel\"><i class=\"icon-cancel\"></i>Zrušit</div><div class=\"fieldImageContainer\">";
 	var start = perPage * (page - 1);
@@ -55,14 +55,15 @@ function renderFieldImageSelector(list: Array<string>, name: string, description
 
 	document.getElementById("fieldImageCancel")!.onclick = function()
 	{
-		addField(name, description, image); // eslint-disable-line @typescript-eslint/no-use-before-define
+		callback(state);
 	}
 	var	imageNodes = document.getElementById("sidePanel")!.getElementsByTagName("img");
 	for(var k = 0; k < imageNodes.length; k++)
 	{
 		imageNodes[k].onclick = function(event: MouseEvent)
 		{
-			addField(name, description, (event.target as HTMLElement).dataset.id); // eslint-disable-line @typescript-eslint/no-use-before-define
+			state.image = (event.target as HTMLElement).dataset.id
+			callback(state);
 		};
 	}
 	var buttonNodes = document.getElementsByClassName("paginationButton");
@@ -70,28 +71,28 @@ function renderFieldImageSelector(list: Array<string>, name: string, description
 	{
 		(buttonNodes[l] as HTMLElement).onclick = function(event): void
 		{
-			openFieldImageSelector(name, description, image, parseInt((event.target as HTMLElement).dataset.page!, 10), perPage);
+			openSidePanelImageSelector(callback, state, parseInt((event.target as HTMLElement).dataset.page!, 10), perPage);
 		};
 	}
 }
 
-function addFiledPayloadBuilder(): Payload
+function addFieldPayloadBuilder(): Payload
 {
 	return {"name": encodeURIComponent((document.getElementById("fieldName") as HTMLInputElement).value), "description": encodeURIComponent((document.getElementById("fieldDescription") as HTMLInputElement).value), "image": encodeURIComponent((document.getElementById("fieldImage") as HTMLInputElement).value)};
 }
 
-function addField(name = "Nová oblast", description = "Popis nové oblasti", image = "00000000-0000-0000-0000-000000000000"): void
+function addField(state = {name: "Nová oblast", description: "Popis nové oblasti", image: "00000000-0000-0000-0000-000000000000"}): void
 {
 	sidePanelOpen();
 	var html = "<div class=\"button yellowButton\" id=\"sidePanelCancel\"><i class=\"icon-cancel\"></i>Zrušit</div>";
 	html += "<div class=\"button greenButton\" id=\"addFieldSave\"><i class=\"icon-floppy\"></i>Uložit</div>";
 	html += "<h3 class=\"sidePanelTitle\">Přidat oblast</h3><form id=\"sidePanelForm\">";
 	html += "<legend for=\"fieldName\">Název:</legend>";
-	html += "<input type=\"text\" class=\"formText formName\" id=\"fieldName\" value=\"" + name + "\" autocomplete=\"off\">";
-	html += "<textarea rows=\"5\" class=\"formText\" id=\"fieldDescription\" autocomplete=\"off\">" + description + "</textarea>";
+	html += "<input type=\"text\" class=\"formText formName\" id=\"fieldName\" value=\"" + state.name + "\" autocomplete=\"off\">";
+	html += "<textarea rows=\"5\" class=\"formText\" id=\"fieldDescription\" autocomplete=\"off\">" + state.description + "</textarea>";
 	html += "<legend for=\"fieldImage\">Náhledový obrázek:</legend>";
-	html += "<input type=\"hidden\" id=\"fieldImage\" value=\"" + image + "\">";
-	html += "<image src=\"" + CONFIG.apiuri + "/image/" + image + "?quality=thumbnail\">";
+	html += "<input type=\"hidden\" id=\"fieldImage\" value=\"" + state.image + "\">";
+	html += "<image src=\"" + CONFIG.apiuri + "/image/" + state.image + "?quality=thumbnail\">";
 	html += "<br><div class=\"button\" id=\"fieldImageChange\"><i class=\"icon-pencil\"></i>Změnit</div>"
 	html += "</form>";
 	document.getElementById("sidePanel")!.innerHTML = html;
@@ -102,10 +103,10 @@ function addField(name = "Nová oblast", description = "Popis nové oblasti", im
 	};
 	document.getElementById("fieldImageChange")!.onclick = function(): void
 	{
-		openFieldImageSelector((document.getElementById("fieldName") as HTMLInputElement).value, (document.getElementById("fieldDescription") as HTMLInputElement).value, image);
+		openSidePanelImageSelector(addField, {name: (document.getElementById("fieldName") as HTMLInputElement).value, description: (document.getElementById("fieldDescription") as HTMLInputElement).value, image: state.image});
 	};
 
-	var aq = new ActionQueue([new Action(CONFIG.apiuri + "/field", "POST", addFiledPayloadBuilder)]);
+	var aq = new ActionQueue([new Action(CONFIG.apiuri + "/field", "POST", addFieldPayloadBuilder)]);
 	document.getElementById("addFieldSave")!.onclick = aq.closeDispatch;
 
 	history.pushState({"sidePanel": "open"}, "title", "/admin/lessons");
