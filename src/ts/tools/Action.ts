2466d87a36e6965b14c8ee6f9641f8680b68423b
Marek Dědič <developer@dedic.eu>
2019-03-08 17:59:22 +0100

    admin-pushed uses ActionCallback

--- src/ts/tools/Action.ts
+++ src/ts/tools/Action.ts
@@ -5,16 +5,42 @@ class Action
 	public url: string;
 	public method: string;
 	public payloadBuilder: () => Payload;
-	public callback: (response: RequestResponse) => void;
+	public callback: ActionCallback;
 	public exceptionHandler: ExceptionHandler;
 
-	public constructor(url: string, method: string, payloadBuilder: () => Payload = function(): Payload {return{};}, callback: (response: RequestResponse) => void = function(): void {}, exceptionHandler: ExceptionHandler = {})
+	public constructor(url: string, method: string, payloadBuilder: () => Payload = function(): Payload {return{};}, callback: ActionCallback = ActionCallback.Nothing, exceptionHandler: ExceptionHandler = {})
 	{
 		this.url = url;
 		this.method = method;
 		this.payloadBuilder = payloadBuilder;
 		this.callback = callback;
 		this.exceptionHandler = exceptionHandler;
+	}
+
+	public runCallback(response: RequestResponse): void
+	{
+		switch(this.callback)
+		{
+			case ActionCallback.DialogConfirm:
+				this.dialogConfirm();
+				break;
+			default:
+				break;
+		}
+	}
+
+	public dialogConfirm(): void
+	{
+		dialog("Akce byla úspěšná.", "OK");
+		refreshMetadata();
+		if(ActionQueueRetry)
+		{
+			showMainView(false);
+		}
+		else
+		{
+			history.back();
+		}
 	}
 
 	public fillID(id: string): void
