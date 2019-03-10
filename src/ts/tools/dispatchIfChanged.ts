2bc4f6d612780ed13bd68814ac22ac72ea8c4560
GenaBitu <genabitu@gmail.com>
2019-03-10 01:04:51 +0100

    Removed some unnecessary API calls

---
+++ src/ts/tools/dispatchIfChanged.ts
@@ -0,0 +1,14 @@
+/* exported dispatchIfChanged */
+
+function dispatchIfChanged(actionQueue: ActionQueue, changed: boolean): void
+{
+	if(changed)
+	{
+		actionQueue.closeDispatch();
+	}
+	else
+	{
+		sidePanelClose();
+		dialog("Akce byla úspěšná.", "OK");
+	}
+}
