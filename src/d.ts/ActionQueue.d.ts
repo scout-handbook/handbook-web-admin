ae9824f06af8d944229e64a08b618769f7a31653
Marek Dědič <developer@dedic.eu>
2022-09-03 12:06:18 +0200

    CLeaned up d.ts files

--- src/d.ts/ActionQueue.d.ts
+++
@@ -1,8 +0,0 @@
-declare class ActionQueue {
-  public actions: Array<Action>;
-  public constructor(actions?: Array<Action>, retry?: boolean);
-  public fillID(id: string): void;
-  public dispatch(background: boolean): void;
-  public defaultDispatch(background: boolean): void;
-  public closeDispatch(): void;
-}
--- src/d.ts/AfterLoadEvent.d.ts
+++
@@ -1,5 +0,0 @@
-declare class AfterLoadEvent {
-  public constructor(threshold: number);
-  public addCallback(callback: (...args: Array<string>) => void): void;
-  public trigger(...args: Array<string>): void;
-}
