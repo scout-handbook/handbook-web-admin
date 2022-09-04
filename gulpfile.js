1f7061312d29fc7c8df50e9a39cf04970cd40df0
Marek Dědič <developer@dedic.eu>
2022-09-04 16:16:32 +0200

    Outputting sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,21 @@ const fs = require("fs");
 const nestedObjectAssign = require("nested-object-assign");
 
 const gulp = require("gulp");
-const uglify = require("uglify-js");
-const composer = require("gulp-uglify/composer");
-const merge = require("merge-stream");
-const sourcemaps = require("gulp-sourcemaps");
-const concat = require("gulp-concat");
+
+const autoprefixer = require("autoprefixer");
 const cleanCSS = require("gulp-clean-css");
+const composer = require("gulp-uglify/composer");
+const concat = require("gulp-concat");
+const htmlmin = require("gulp-htmlmin");
+const inject = require("gulp-inject-string");
+const merge = require("merge-stream");
 const postcss = require("gulp-postcss");
 const postcssCustomProperties = require("postcss-custom-properties");
 const rename = require("gulp-rename");
-const autoprefixer = require("autoprefixer");
-const inject = require("gulp-inject-string");
-const htmlmin = require("gulp-htmlmin");
+const sourcemaps = require("gulp-sourcemaps");
+const through = require('through2');
 const ts = require("gulp-typescript");
+const uglify = require("uglify-js");
 const webpack = require("webpack-stream");
 
 const minify = composer(uglify, console);
@@ -52,27 +54,31 @@ gulp.task("build:html", function () {
 });
 
 gulp.task("build:js", function () {
-  // TODO: sourcemaps - check that they're still written
-  // TODO: Minify
   function bundle(name, addConfig = false) {
     let ret = gulp
       .src("src/ts/" + name + ".ts")
-      .pipe(webpack(require("./webpack.config.js")));
+      .pipe(webpack(require("./webpack.config.js")))
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(
+        through.obj(function (file, _, cb) {
+          const isSourceMap = /\.map$/.test(file.path);
+          if (!isSourceMap) this.push(file);
+          cb();
+        })
+      );
     if (addConfig) {
       ret = ret.pipe(
         inject.prepend(
-          '"use strict";\nvar CONFIG = JSON.parse(\'' +
-            JSON.stringify(getConfig()) +
-            "');\n"
+          "var CONFIG = JSON.parse('" + JSON.stringify(getConfig()) + "');\n"
         )
       );
     }
-    return ret.pipe(rename(name + ".min.js")).pipe(gulp.dest("dist/"));
+    return ret
+      .pipe(rename({ basename: name, suffix: ".min" }))
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest("dist/"));
   }
-  return merge(
-    bundle("admin", true),
-    bundle("admin-worker")
-  );
+  return merge(bundle("admin", true), bundle("admin-worker"));
 });
 
 gulp.task("build:css", function () {
