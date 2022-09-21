8e9d776d5340eb7a33f50fdaf958a9f460277349
Marek Dědič <developer@dedic.eu>
2022-09-21 14:18:49 +0200

    Using svelte-navigator for the main view

---
+++ src/ts/admin/MainView.svelte
@@ -0,0 +1,9 @@
+<div></div>
+
+<script lang="ts">
+import { setMainPageTab, showMainView } from "./views/main";
+if (window.location.pathname.substring(7)) {
+setMainPageTab(window.location.pathname.substring(7) as MainPageTab);
+}
+showMainView(false);
+</script>
