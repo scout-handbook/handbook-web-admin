538aedf30fb2a6c753c235e7263ad4c6c799684d
Marek Dědič <developer@dedic.eu>
2022-09-21 14:23:10 +0200

    Moved svelte files into own dir

--- src/svelte/admin/MainView.svelte
+++ src/svelte/admin/MainView.svelte
@@ -1,7 +1,7 @@
 <div></div>
 
 <script lang="ts">
-import { setMainPageTab, showMainView } from "./views/main";
+import { setMainPageTab, showMainView } from "../../ts/admin/views/main";
 if (window.location.pathname.substring(7)) {
 setMainPageTab(window.location.pathname.substring(7) as MainPageTab);
 }
