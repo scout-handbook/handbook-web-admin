2e68030425c283b429e16c7eb0262fdebe61d311
Marek Dědič <developer@dedic.eu>
2022-09-24 20:35:30 +0200

    Fixed EditLessonView

---
+++ src/svelte/admin/LoadingIndicator.svelte
@@ -0,0 +1,21 @@
+<div style:display={$loadingIndicatorVisible ? "block" : "none"} />
+
+<script lang="ts">
+  import { loadingIndicatorVisible } from "../../ts/admin/stores";
+</script>
+
+<style>
+  div {
+    animation: spin 2s linear infinite;
+    border: 16px solid var(--background-darker);
+    border-radius: 50%;
+    border-top: 16px solid var(--accent-color);
+    height: 120px;
+    left: 50%;
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 120px;
+    z-index: 9;
+  }
+</style>
--- src/ts/admin/stores.ts
+++ src/ts/admin/stores.ts
@@ -1,5 +1,7 @@
-import { readable } from "svelte/store";
+import { readable, writable } from "svelte/store";
 
-export const admin_uri = readable(CONFIG["admin-uri"]);
-export const frontend_uri = readable(CONFIG["frontend-uri"]);
-export const site_name = readable(CONFIG["site-name"]);
+export const admin_uri = readable<string>(CONFIG["admin-uri"]);
+export const frontend_uri = readable<string>(CONFIG["frontend-uri"]);
+export const site_name = readable<string>(CONFIG["site-name"]);
+
+export const loadingIndicatorVisible = writable<boolean>(false);
