e414a77fbdf15a33b7a73a75fdce919efae75de8
Marek Dědič <developer@dedic.eu>
2022-09-25 21:04:24 +0200

    Converted LessonEditor component to true svelte

--- src/svelte/admin/LessonEditor.svelte
+++ src/svelte/admin/LessonEditor.svelte
@@ -1,4 +1,46 @@
+<div id="side-panel"></div>
+<div id="side-panel-overlay"></div>
+<header>
+  <div class="button yellow-button" id="discard" on:click={() => {editorDiscard(discardActionQueue);}}>
+    <i class="icon-cancel"></i>
+    Zrušit
+  </div>
+  <form>
+    <input type="text" class="form-text form-name" id="name" value={name} autocomplete="off" on:input={() => {editorOnChange(refreshAction);}} on:change={() => {editorOnChange(refreshAction);}}>
+  </form>
+  <div class="button green-button" id="save" on:click={saveCallback}>
+    <i class="icon-floppy"></i>
+    Uložit
+  </div>
+  <div class="button" id="lesson-settings" on:click={() => {lessonSettings(id, saveActionQueue, false);}}>
+    <i class="icon-cog"></i>
+    Nastavení
+  </div>
+</header>
+<div id="image-selector">
+  <div id="image-scroller">
+    <div class="button yellow-button" id="close-image-selector" on:click={toggleImageSelector}>
+      <i class="icon-up-open"></i>
+      Zavřít
+    </div>
+    <!-- TODO: Re-enable uploads in editor without discarding its contents
+    <div class="button green-button" on:click={() => {addImage(true);}}>
+      <i class="icon-plus"></i>
+      Nahrát
+    </div>
+    -->
+    <div id="image-wrapper"></div>
+  </div>
+</div>
+<div id="editor">
+  <textarea></textarea>
+</div>
+<div id="preview">
+  <div id="preview-inner"></div>
+</div>
+
 <script lang="ts">
+  import { onMount } from "svelte";
   import { navigate } from "svelte-navigator";
 
   import { ActionQueue } from "../../ts/admin/tools/ActionQueue";
@@ -17,45 +59,9 @@
 
   populateEditorCache(id);
   setChanged(false);
-  const html =
-    '\
-<div id="side-panel"></div><div id="side-panel-overlay"></div>\
-<header>\
-    <div class="button yellow-button" id="discard">\
-        <i class="icon-cancel"></i>Zrušit\
-    </div>\
-    <form>\
-        <input type="text" class="form-text form-name" id="name" value="' +
-    name +
-    '" autocomplete="off">\
-    </form>\
-    <div class="button green-button" id="save">\
-        <i class="icon-floppy"></i>Uložit\
-    </div>\
-    <div class="button" id="lesson-settings">\
-        <i class="icon-cog"></i>Nastavení\
-    </div>\
-</header>\
-<div id="image-selector">\
-    <div id="image-scroller">\
-        <div class="button yellow-button" id="close-image-selector">\
-            <i class="icon-up-open"></i> Zavřít\
-        </div>\
-        <div class="button green-button" id="imageSelectorAdd">\
-            <i class="icon-plus"></i> Nahrát\
-        </div>\
-        <div id="image-wrapper"></div>\
-    </div>\
-</div>\
-<div id="editor"><textarea></textarea></div><div id="preview"><div id="preview-inner"></div></div>';
-
-  document.getElementsByTagName("main")[0].innerHTML = html;
   refreshPreview(name, body, "preview-inner");
 
-  document.getElementById("discard")!.onclick = function (): void {
-    editorDiscard(discardActionQueue);
-  };
-  document.getElementById("save")!.onclick = function (): void {
+  function saveCallback(): void {
     if (changed) {
       saveActionQueue.defaultDispatch(false);
     } else {
@@ -63,90 +69,78 @@
       discardActionQueue.defaultDispatch(false);
     }
   };
-  document.getElementById("lesson-settings")!.onclick = function (): void {
-    lessonSettings(id, saveActionQueue, false);
-  };
-  document.getElementById("close-image-selector")!.onclick =
-    toggleImageSelector;
-  //document.getElementById("imageSelectorAdd")!.onclick = function(): void {addImage(true);};
-  document.getElementById("imageSelectorAdd")!.style.display = "none"; // TODO: Re-enable uploads in editor without discarding its contents
 
-  setEditor(new EasyMDE({
-    autoDownloadFontAwesome: false,
-    autofocus: true,
-    element: document.getElementById("editor")!.firstChild as HTMLElement,
-    indentWithTabs: false,
-    parsingConfig: {
-      allowAtxHeaderWithoutSpace: true,
-    },
-    spellChecker: false,
-    status: false,
-    tabSize: 4,
-    toolbar: [
-      {
-        name: "bold",
-        action: EasyMDE.toggleBold,
-        className: "icon-bold",
-        title: "Tučné",
+  onMount(() => {
+    setEditor(new EasyMDE({
+      autoDownloadFontAwesome: false,
+      autofocus: true,
+      element: document.getElementById("editor")!.firstChild as HTMLElement,
+      indentWithTabs: false,
+      parsingConfig: {
+        allowAtxHeaderWithoutSpace: true,
       },
-      {
-        name: "italic",
-        action: EasyMDE.toggleItalic,
-        className: "icon-italic",
-        title: "Kurzíva",
-      },
-      {
-        name: "heading",
-        action: EasyMDE.toggleHeadingSmaller,
-        className: "icon-header",
-        title: "Nadpis",
-      },
-      "|",
-      {
-        name: "unordered-list",
-        action: EasyMDE.toggleUnorderedList,
-        className: "icon-list-bullet",
-        title: "Seznam s odrážkami",
-      },
-      {
-        name: "ordered-list",
-        action: EasyMDE.toggleOrderedList,
-        className: "icon-list-numbered",
-        title: "Číslovaný seznam",
-      },
-      "|",
-      {
-        name: "link",
-        action: EasyMDE.drawLink,
-        className: "icon-link",
-        title: "Vložit odkaz",
-      },
-      {
-        name: "image",
-        action: toggleImageSelector,
-        className: "icon-picture",
-        title: "Vložit obrázek",
-      },
-      {
-        name: "table",
-        action: EasyMDE.drawTable,
-        className: "icon-table",
-        title: "Vložit tabulku",
-      },
-    ],
-  }));
-  editor.value(body);
-  editor.codemirror.getDoc().clearHistory();
-  editor.codemirror.on("change", function (): void {
-    editorOnChange(refreshAction);
+      spellChecker: false,
+      status: false,
+      tabSize: 4,
+      toolbar: [
+        {
+          name: "bold",
+          action: EasyMDE.toggleBold,
+          className: "icon-bold",
+          title: "Tučné",
+        },
+        {
+          name: "italic",
+          action: EasyMDE.toggleItalic,
+          className: "icon-italic",
+          title: "Kurzíva",
+        },
+        {
+          name: "heading",
+          action: EasyMDE.toggleHeadingSmaller,
+          className: "icon-header",
+          title: "Nadpis",
+        },
+        "|",
+        {
+          name: "unordered-list",
+          action: EasyMDE.toggleUnorderedList,
+          className: "icon-list-bullet",
+          title: "Seznam s odrážkami",
+        },
+        {
+          name: "ordered-list",
+          action: EasyMDE.toggleOrderedList,
+          className: "icon-list-numbered",
+          title: "Číslovaný seznam",
+        },
+        "|",
+        {
+          name: "link",
+          action: EasyMDE.drawLink,
+          className: "icon-link",
+          title: "Vložit odkaz",
+        },
+        {
+          name: "image",
+          action: toggleImageSelector,
+          className: "icon-picture",
+          title: "Vložit obrázek",
+        },
+        {
+          name: "table",
+          action: EasyMDE.drawTable,
+          className: "icon-table",
+          title: "Vložit tabulku",
+        },
+      ],
+    }));
+    editor.value(body);
+    editor.codemirror.getDoc().clearHistory();
+    editor.codemirror.on("change", function (): void {
+      editorOnChange(refreshAction);
+    });
   });
-
-  document.getElementById("name")!.oninput = function (): void {
-    editorOnChange(refreshAction);
-  };
-  document.getElementById("name")!.onchange = function (): void {
-    editorOnChange(refreshAction);
-  };
 
   prepareImageSelector();
 </script>
