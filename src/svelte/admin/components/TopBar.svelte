5da6cda90252003885c5b8eb5b55824dcf04bb63
Marek Dědič <developer@dedic.eu>
2022-09-26 19:30:22 +0200

    Split out UserAccountBox

--- src/svelte/admin/components/TopBar.svelte
+++ src/svelte/admin/components/TopBar.svelte
@@ -1,44 +1,9 @@
 <script lang="ts">
-  import { config, loginstate } from "../../../ts/admin/stores";
+  import UserAccountBox from "./UserAccountBox.svelte";
 </script>
 
 <div id="top-bar">
-  <div id="user-account">
-    {#await $loginstate}
-      <img
-        id="user-avatar"
-        alt="Account avatar"
-        src={$config['admin-uri'] + "/avatar.png"}
-      />
-    {:then state}
-      {#if state.avatar}
-        <img
-          id="user-avatar"
-          alt="Account avatar"
-          src={"data:image/png;base64," + state.avatar}
-        />
-      {:else}
-        <img
-          id="user-avatar"
-          alt="Account avatar"
-          src={$config['admin-uri'] + "/avatar.png"}
-        />
-      {/if}
-    {/await}
-    <div id="user-name">&nbsp;</div>
-    <div id="log-link">
-      <a
-        href={$config[
-          'admin-uri'
-        ] + "/v1.0/logout?redirect-uri=" + encodeURIComponent(
-          $config['frontend-uri']
-        )}
-      >
-        Odhlásit
-      </a>
-      <a id="frontend-link" href="/">Zpět na web</a>
-    </div>
-  </div>
+  <UserAccountBox />
   <div id="lesson-manager" class="top-bar-tab">Lekce</div>
   <div id="competence-manager" class="top-bar-tab">Kompetence</div>
   <div id="image-manager" class="top-bar-tab">Obrázky</div>
