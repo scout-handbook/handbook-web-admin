6206251124d361bbae3b441e4666279c1bde3720
Marek Dědič <developer@dedic.eu>
2022-09-26 19:30:22 +0200

    Split out UserAccountBox

---
+++ src/svelte/admin/components/UserAccountBox.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { config, loginstate } from "../../../ts/admin/stores";
+</script>
+
+<div id="user-account">
+  {#await $loginstate}
+    <img
+      id="user-avatar"
+      alt="Account avatar"
+      src={$config['admin-uri'] + "/avatar.png"}
+    />
+  {:then state}
+    {#if state.avatar}
+      <img
+        id="user-avatar"
+        alt="Account avatar"
+        src={"data:image/png;base64," + state.avatar}
+      />
+    {:else}
+      <img
+        id="user-avatar"
+        alt="Account avatar"
+        src={$config['admin-uri'] + "/avatar.png"}
+      />
+    {/if}
+  {/await}
+  <div id="user-name">&nbsp;</div>
+  <div id="log-link">
+    <a
+      href={$config[
+        'admin-uri'
+      ] + "/v1.0/logout?redirect-uri=" + encodeURIComponent(
+        $config['frontend-uri']
+      )}
+    >
+      Odhlásit
+    </a>
+    <a id="frontend-link" href="/">Zpět na web</a>
+  </div>
+</div>
