b14e9ff8827b123899acd4d57829f89660705c4b
Marek Dědič <developer@dedic.eu>
2022-09-30 15:49:49 +0200

    Moved showImagePreview into ImageView

--- src/svelte/admin/ImageView.svelte
+++ src/svelte/admin/ImageView.svelte
@@ -9,7 +9,6 @@
   import { refreshLogin } from "../../ts/admin/tools/refreshLogin";
   import { RequestResponse } from "../../ts/admin/interfaces/RequestResponse";
   import { renderPagination } from "../../ts/admin/UI/pagination";
-  import { showImagePreview } from "../../ts/admin/views/mainSubviews/image";
 
   let page = 1;
   const perPage = 15;
@@ -51,6 +50,25 @@
         }
       }, 100);
     });
+  }
+
+  function showImagePreview(event: MouseEvent): void {
+    const overlay = document.getElementById("overlay")!;
+    overlay.style.display = "inline";
+    overlay.style.cursor = "pointer";
+    const html =
+      '<img src="' +
+      CONFIG["api-uri"] +
+      "/v1.0/image/" +
+      (event.target as HTMLElement).dataset.id! +
+      '" class="preview-image">';
+    overlay.innerHTML = html;
+    overlay.onclick = function (): void {
+      overlay.style.display = "none";
+      overlay.style.cursor = "auto";
+      overlay.innerHTML = "";
+      overlay.onclick = null;
+    };
   }
 
   fixNavigation();
