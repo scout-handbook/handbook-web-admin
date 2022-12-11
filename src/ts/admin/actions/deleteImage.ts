5eb78b499472cfb19ed4c4daa56a113886bc8cb0
Marek Dědič <developer@dedic.eu>
2022-12-11 15:49:04 +0100

    Using Button component in ImageView

--- src/ts/admin/actions/deleteImage.ts
+++ src/ts/admin/actions/deleteImage.ts
@@ -1,15 +1,14 @@
 import { Action } from "../tools/Action";
 import { ActionQueue } from "../tools/ActionQueue";
 import { dialog } from "../UI/dialog";
-import { getAttribute } from "../UI/button";
 import { refreshLogin } from "../tools/refreshLogin";
 
-export function deleteImageOnClick(event: MouseEvent): void {
+export function deleteImageOnClick(id: string): void {
   const aq = new ActionQueue([
     new Action(
       CONFIG["api-uri"] +
         "/v1.0/image/" +
-        encodeURIComponent(getAttribute(event, "id")),
+        encodeURIComponent(id),
       "DELETE"
     ),
   ]);
