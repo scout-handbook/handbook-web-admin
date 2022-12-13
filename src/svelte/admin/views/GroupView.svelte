f2261f711f214fb409c9017bd37df4798eebdd8c
Marek Dědič <developer@dedic.eu>
2022-12-13 14:19:04 +0100

    Adding groups with svelte

--- src/svelte/admin/views/GroupView.svelte
+++ src/svelte/admin/views/GroupView.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import { addGroup } from "../../../ts/admin/actions/addGroup";
+  import { useLocation, useNavigate } from "svelte-navigator";
+
+  import AddGroupPanel from "../components/action-modals/AddGroupPanel.svelte";
   import Button from "../components/Button.svelte";
   import { changeGroupOnClick } from "../../../ts/admin/actions/changeGroup";
   import { deleteGroupOnClick } from "../../../ts/admin/actions/deleteGroup";
@@ -12,15 +14,32 @@
 
   export let groups: IDList<Group>;
   export let loginstate: Loginstate;
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  $: action = $location.state?.action;
+
   $: adminPermissions =
     loginstate.role === "administrator" || loginstate.role === "superuser";
 
   refreshLogin(true);
 </script>
 
+{#if action === "add-group"}
+  <AddGroupPanel />
+{/if}
+
 <h1>{$siteName + " - Uživatelské skupiny"}</h1>
 {#if adminPermissions}
-  <Button green icon="plus" on:click={addGroup}>Přidat</Button>
+  <Button
+    green
+    icon="plus"
+    on:click={() => {
+      navigate("/groups", { state: { action: "add-group" } });
+    }}
+  >
+    Přidat
+  </Button>
 {/if}
 {#each groups.asArray() as { id, value: group }}
   {#if id === "00000000-0000-0000-0000-000000000000"}
