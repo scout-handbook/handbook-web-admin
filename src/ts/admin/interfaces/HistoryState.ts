a4b44c86d5ca22b866c8aecd8201016aedf6138d
Marek Dědič <developer@dedic.eu>
2023-01-02 00:54:14 +0100

    Removed legacy history handler

--- src/ts/admin/interfaces/HistoryState.ts
+++
@@ -1,8 +0,0 @@
-import { MainPageTab } from "./MainPageTab";
-
-export interface HistoryState {
-  id?: string;
-  page?: MainPageTab;
-  sidePanelImageSelectorAction?: string;
-  sidePanelImageSelectorState?: Record<string, string>;
-}
