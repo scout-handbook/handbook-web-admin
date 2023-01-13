6a07a76297c179784d7d95b23cc63b63238df4fc
Marek Dědič <developer@dedic.eu>
2023-01-13 21:17:00 +0100

    Deleted obsolete getAttribute() function

--- src/ts/admin/UI/button.ts
+++
@@ -1,7 +0,0 @@
-export function getAttribute(event: MouseEvent, attribute: string): string {
-  let el = event.target as HTMLElement;
-  while (!Object.prototype.hasOwnProperty.call(el.dataset, attribute)) {
-    el = el.parentElement!;
-  }
-  return el.dataset[attribute]!;
-}
