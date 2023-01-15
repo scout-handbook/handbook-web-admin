38207f432da62f8b6f0c955be635fa6fc75dace9
Marek Dědič <developer@dedic.eu>
2023-01-15 18:31:54 +0100

    Fixed missing Config

---
+++ src/ts/admin/interfaces/Config.ts
@@ -0,0 +1,26 @@
+export interface ConfigCustomProperties {
+  "--accent-color": string;
+  "--avatar-border": string;
+  "--background-darker": string;
+  "--background-darkest": string;
+  "--border-color": string;
+  "--button-accent-cyan": string;
+  "--button-accent-green": string;
+  "--button-accent-red": string;
+  "--button-accent-yellow": string;
+  "--button-border": string;
+  "--nav-width": string;
+  "--non-accented-link-color": string;
+  "--overlay-color": string;
+  "--overlay-fallback-color": string;
+  "--table-border": string;
+  "--table-odd-row-background": string;
+}
+
+export interface Config {
+  "api-uri": string;
+  "admin-uri": string;
+  "frontend-uri": string;
+  "site-name": string;
+  "custom-properties": ConfigCustomProperties;
+}
