e886eabc031603d125f54a51e84c1ed51f6b7c51
Marek Dědič <developer@dedic.eu>
2023-01-15 11:42:34 +0100

    Replaced URLSearchParams with custom getQueryField

---
+++ src/ts/admin/tools/getQueryField.ts
@@ -0,0 +1,9 @@
+export function getQueryField(search: string, key: string): string | null {
+  const keyValuePair = new RegExp("[?&]" + key + "(=([^&#]*)|&|#|$)").exec(
+    search
+  );
+  if (!keyValuePair || !keyValuePair[2]) {
+    return null;
+  }
+  return decodeURIComponent(keyValuePair[2].replace(/\+/g, " "));
+}
