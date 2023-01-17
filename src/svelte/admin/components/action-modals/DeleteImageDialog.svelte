f60e65af6da7cac266e0eec66481fb918c68e20f
Marek Dědič <developer@dedic.eu>
2023-01-17 17:45:01 +0100

    Using Promise confirmation in DeleteImageDialog

--- src/svelte/admin/components/action-modals/DeleteImageDialog.svelte
+++ src/svelte/admin/components/action-modals/DeleteImageDialog.svelte
@@ -5,31 +5,38 @@
   import { Action } from "../../../../ts/admin/tools/Action";
   import { ActionQueue } from "../../../../ts/admin/tools/ActionQueue";
   import { refreshLogin } from "../../../../ts/admin/tools/refreshLogin";
+  import ConfirmationDialog from "../ConfirmationDialog.svelte";
   import Dialog from "../Dialog.svelte";
 
   export let payload: { imageId: string };
 
   const navigate = useNavigate();
 
+  let confirmPromise: Promise<void> | null = null;
+
   refreshLogin();
 
   function confirmCallback() {
-    new ActionQueue([
+    confirmPromise = new ActionQueue([
       new Action(
         $apiUri + "/v1.0/image/" + encodeURIComponent(payload.imageId),
         "DELETE"
       ),
-    ]).defaultDispatch();
+    ]).dispatch();
   }
 </script>
 
-<Dialog
-  confirmButtonText="Ano"
-  dismissButtonText="Ne"
-  on:confirm={confirmCallback}
-  on:dismiss={() => {
-    navigate(-1);
-  }}
->
-  Opravdu si přejete smazat tento obrázek?
-</Dialog>
+{#if confirmPromise !== null}
+  <ConfirmationDialog {confirmPromise} />
+{:else}
+  <Dialog
+    confirmButtonText="Ano"
+    dismissButtonText="Ne"
+    on:confirm={confirmCallback}
+    on:dismiss={() => {
+      navigate(-1);
+    }}
+  >
+    Opravdu si přejete smazat tento obrázek?
+  </Dialog>
+{/if}
