c22a7248875b0530e85fcf9429b39e46ba907668
Marek Dědič <developer@dedic.eu>
2023-01-17 19:38:04 +0100

    Using Promise confirmation in AddCompetencePanel

--- src/svelte/admin/components/action-modals/AddCompetencePanel.svelte
+++ src/svelte/admin/components/action-modals/AddCompetencePanel.svelte
@@ -6,6 +6,7 @@
   import { ActionQueue } from "../../../../ts/admin/tools/ActionQueue";
   import { refreshLogin } from "../../../../ts/admin/tools/refreshLogin";
   import Button from "../Button.svelte";
+  import DoneDialog from "../DoneDialog.svelte";
   import SidePanel from "../SidePanel.svelte";
 
   const navigate = useNavigate();
@@ -13,56 +14,61 @@
   let number = "00";
   let name = "Nová kompetence";
   let description = "Popis nové kompetence";
+  let donePromise: Promise<void> | null = null;
 
   refreshLogin();
 
   function saveCallback() {
-    new ActionQueue([
+    donePromise = new ActionQueue([
       new Action($apiUri + "/v1.0/competence", "POST", {
         number: encodeURIComponent(number),
         name: encodeURIComponent(name),
         description: encodeURIComponent(description),
       }),
-    ]).defaultDispatch();
+    ]).dispatch();
   }
 </script>
 
-<SidePanel>
-  <Button
-    icon="cancel"
-    yellow
-    on:click={() => {
-      navigate(-1);
-    }}
-  >
-    Zrušit
-  </Button>
-  <Button green icon="floppy" on:click={saveCallback}>Uložit</Button>
-  <h3 class="side-panel-title">Přidat kompetenci</h3>
-  <form id="side-panel-form">
-    <span class="competence-heading">Kompetence</span>
-    <input
-      id="competence-number"
-      class="form-text form-name"
-      autocomplete="off"
-      type="text"
-      bind:value={number}
-    />
-    <br />
-    <input
-      id="competence-name"
-      class="form-text"
-      autocomplete="off"
-      type="text"
-      bind:value={name}
-    />
-    <br />
-    <textarea
-      id="competence-description"
-      class="form-text"
-      autocomplete="off"
-      rows="5"
-      bind:value={description}
-    />
-  </form>
-</SidePanel>
+{#if donePromise !== null}
+  <DoneDialog {donePromise} />
+{:else}
+  <SidePanel>
+    <Button
+      icon="cancel"
+      yellow
+      on:click={() => {
+        navigate(-1);
+      }}
+    >
+      Zrušit
+    </Button>
+    <Button green icon="floppy" on:click={saveCallback}>Uložit</Button>
+    <h3 class="side-panel-title">Přidat kompetenci</h3>
+    <form id="side-panel-form">
+      <span class="competence-heading">Kompetence</span>
+      <input
+        id="competence-number"
+        class="form-text form-name"
+        autocomplete="off"
+        type="text"
+        bind:value={number}
+      />
+      <br />
+      <input
+        id="competence-name"
+        class="form-text"
+        autocomplete="off"
+        type="text"
+        bind:value={name}
+      />
+      <br />
+      <textarea
+        id="competence-description"
+        class="form-text"
+        autocomplete="off"
+        rows="5"
+        bind:value={description}
+      />
+    </form>
+  </SidePanel>
+{/if}
