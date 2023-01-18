3eafeb7e537679d4bb11ebbe2e73d24a53b734df
Marek Dědič <developer@dedic.eu>
2023-01-18 14:36:08 +0100

    RestoreLessonView handles its own loading

--- src/svelte/admin/views/RestoreLessonView.svelte
+++ src/svelte/admin/views/RestoreLessonView.svelte
@@ -4,13 +4,13 @@
   import { RequestResponse } from "../../../ts/admin/interfaces/RequestResponse";
   import { setChanged } from "../../../ts/admin/lessonEditor/editor";
   import { apiUri } from "../../../ts/admin/stores";
-  import { loadingIndicatorVisible } from "../../../ts/admin/stores";
   import { Action } from "../../../ts/admin/tools/Action";
   import { ActionCallback } from "../../../ts/admin/tools/ActionCallback";
   import { ActionQueue } from "../../../ts/admin/tools/ActionQueue";
   import { getQueryField } from "../../../ts/admin/tools/getQueryField";
   import { authFailHandler, request } from "../../../ts/admin/tools/request";
   import LessonEditor from "../components/LessonEditor.svelte";
+  import LoadingIndicator from "../components/LoadingIndicator.svelte";
 
   export let lessonID: string;
   export let version: string;
@@ -31,23 +31,26 @@
     ),
   ]);
 
-  loadingIndicatorVisible.set(true);
-  request(
-    $apiUri + "/v1.0/deleted-lesson/" + lessonID + "/history/" + version,
-    "GET",
-    {},
-    function (response: RequestResponse): void {
-      body = response as string;
-      loadingIndicatorVisible.set(false);
-      // TODO: Remove this horrible hack
-      setTimeout(() => {
-        setChanged();
-      }, 100);
-    },
-    authFailHandler
-  );
+  let bodyPromise = new Promise<void>((resolve) => {
+    request(
+      $apiUri + "/v1.0/deleted-lesson/" + lessonID + "/history/" + version,
+      "GET",
+      {},
+      function (response: RequestResponse): void {
+        body = response as string;
+        resolve();
+        // TODO: Remove this horrible hack
+        setTimeout(() => {
+          setChanged();
+        }, 100);
+      },
+      authFailHandler
+    );
+  });
 </script>
 
-{#if !$loadingIndicatorVisible}
+{#await bodyPromise}
+  <LoadingIndicator />
+{:then}
   <LessonEditor id={null} {saveActionQueue} bind:body bind:lessonName={name} />
-{/if}
+{/await}
