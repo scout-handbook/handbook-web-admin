052c059ebaa43acb129564157dd68cba26d61fb8
Marek Dědič <developer@dedic.eu>
2023-01-18 17:27:39 +0100

    Split ImageSelector from LessonEditor

---
+++ src/svelte/admin/components/LessonEditor/ImageSelector.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import {
+    prepareImageSelector,
+    toggleImageSelector,
+  } from "../../../../ts/admin/lessonEditor/imageSelector";
+  import Button from "../../components/Button.svelte";
+
+  prepareImageSelector();
+</script>
+
+<div id="image-selector">
+  <div id="image-scroller">
+    <Button icon="up-open" yellow on:click={toggleImageSelector}>Zavřít</Button>
+    <!-- TODO: Re-enable uploads in editor without discarding its contents
+    <Button
+      icon="plus"
+      green
+      on:click={() => {
+        addImage(true); // Removed
+      }}>
+      Nahrát
+    </Button>
+    -->
+    <div id="image-wrapper" />
+  </div>
+</div>
