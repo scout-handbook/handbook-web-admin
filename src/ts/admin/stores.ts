9db2310713b89b7ebe3e453654698047a145c586
Marek Dědič <developer@dedic.eu>
2023-01-22 20:16:43 +0100

    Removed the loginstate store

--- src/ts/admin/stores.ts
+++ src/ts/admin/stores.ts
@@ -6,7 +6,6 @@ import type { ConfigCustomProperties } from "./interfaces/Config";
 import type { Field } from "./interfaces/Field";
 import type { Group } from "./interfaces/Group";
 import type { Lesson } from "./interfaces/Lesson";
-import type { Loginstate } from "./interfaces/Loginstate";
 
 export const adminUri = readable<string>(CONFIG["admin-uri"]);
 export const apiUri = readable<string>(CONFIG["api-uri"]);
@@ -20,7 +19,6 @@ export const fields = writable<IDList<Field> | null>(null);
 export const competences = writable<IDList<Competence> | null>(null);
 export const groups = writable<IDList<Group> | null>(null);
 export const lessons = writable<IDList<Lesson> | null>(null);
-export const loginstate = writable<Loginstate | null>(null);
 
 export const globalLoadingIndicator = writable<boolean>(false);
 export const globalDialogMessage = writable<string | null>(null);
