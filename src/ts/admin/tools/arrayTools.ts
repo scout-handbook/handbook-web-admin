b210e7f68bf0f91b970ca7839e671fd5ff12d59e
Marek Dědič <developer@dedic.eu>
2023-01-27 22:31:39 +0100

    Split out group processing

--- src/ts/admin/tools/arrayTools.ts
+++ src/ts/admin/tools/arrayTools.ts
@@ -9,6 +9,6 @@ export function map<T>(arr: Array<[string, T]>, fn: (item: T) => T): void {
 export function sort<T>(
   arr: Array<[string, T]>,
   comparator: (first: T, second: T) => number
-): void {
-  arr.sort(([_1, first], [_2, second]) => comparator(first, second));
+): Array<[string, T]> {
+  return arr.sort(([_1, first], [_2, second]) => comparator(first, second));
 }
