85a239d62b5e406e98d6515f30e36074df1330bc
Marek Dědič <developer@dedic.eu>
2023-01-27 12:49:07 +0100

    Added arrayTools sort function

--- src/ts/admin/tools/arrayTools.ts
+++ src/ts/admin/tools/arrayTools.ts
@@ -1,3 +1,10 @@
 export function get<T>(arr: Array<[string, T]>, id: string): T | undefined {
   return arr.find(([elementId, _]) => elementId === id)?.[1];
 }
+
+export function sort<T>(
+  arr: Array<[string, T]>,
+  comparator: (first: T, second: T) => number
+): void {
+  arr.sort(([_1, first], [_2, second]) => comparator(first, second));
+}
