a6985c73588e17adf4c07f50a48d0e90e3c4f4bb
Marek Dědič <developer@dedic.eu>
2023-01-30 18:21:31 +0100

    Using SWR in RestoreLessonVersion

--- src/ts/admin/tools/arrayTools.ts
+++ src/ts/admin/tools/arrayTools.ts
@@ -2,8 +2,11 @@ export function get<T>(arr: Array<[string, T]>, id: string): T | undefined {
   return arr.find(([elementId, _]) => elementId === id)?.[1];
 }
 
-export function map<T>(arr: Array<[string, T]>, fn: (item: T) => T): void {
-  arr.map(([id, value]) => [id, fn(value)]);
+export function map<T>(
+  arr: Array<[string, T]>,
+  fn: (item: T) => T
+): Array<[string, T]> {
+  return arr.map(([id, value]) => [id, fn(value)]);
 }
 
 export function sort<T>(
