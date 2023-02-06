f9598da8f5a006db73e2a4fe8cab814da8a1b91c
Marek Dědič <developer@dedic.eu>
2023-02-06 17:23:15 +0100

    Added LessonProvider

---
+++ src/svelte/admin/components/swr-wrappers/LessonProvider.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" strictEvents>
+  import { useSWR } from "sswr";
+  import { derived } from "svelte/store";
+
+  import type { Competence } from "../../../../ts/admin/interfaces/Competence";
+  import type { Lesson } from "../../../../ts/admin/interfaces/Lesson";
+  import {
+    processCompetences,
+    processLessons,
+  } from "../../../../ts/admin/metadata";
+  import { constructURL } from "../../../../ts/admin/tools/constructURL";
+  import LoadingIndicator from "../LoadingIndicator.svelte";
+
+  interface $$Slots {
+    default: {
+      competences: Array<[string, Competence]>;
+      lessons: Array<[string, Lesson]>;
+    };
+  }
+
+  const competences = derived(
+    useSWR<Record<string, Competence>>(constructURL("v1.0/competence")).data,
+    processCompetences,
+    undefined
+  );
+  const lessons = derived(
+    [
+      useSWR<Record<string, Lesson>>(
+        constructURL("v1.0/lesson?override-group=true")
+      ).data,
+      competences,
+    ],
+    processLessons,
+    undefined
+  );
+</script>
+
+{#if $competences === undefined || $lessons === undefined}
+  <LoadingIndicator />
+{:else}
+  <slot competences={$competences} lessons={$lessons} />
+{/if}
