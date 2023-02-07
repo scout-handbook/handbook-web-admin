653d43594b128c5985f43a44b80418e32ab91c51
Marek Dědič <developer@dedic.eu>
2023-02-07 15:40:36 +0100

    Using reactivity for SidePanelImageSelector

--- src/svelte/admin/components/SidePanelImageSelector.svelte
+++ src/svelte/admin/components/SidePanelImageSelector.svelte
@@ -7,26 +7,21 @@
   import Button from "./Button.svelte";
   import DoubleSidePanel from "./DoubleSidePanel.svelte";
   import LoadingIndicator from "./LoadingIndicator.svelte";
+  import Pagination from "./Pagination.svelte";
 
   const dispatch = createEventDispatcher<{ cancel: never; select: string }>();
 
   let page = 1;
   const perPage = 15;
 
-  let imageListPromise: Promise<Array<string>>;
-
-  // TODO: Reactive variable?
   // TODO: SWR?
-  function reload(): void {
-    imageListPromise = request<Array<string>>(
-      $apiUri + "/v1.0/image",
-      "GET",
-      {},
-      reAuthHandler
-    ).then((response) => response.slice(perPage * (page - 1), perPage * page));
-  }
+  const imageListPromise = request<Array<string>>(
+    $apiUri + "/v1.0/image",
+    "GET",
+    {},
+    reAuthHandler
+  );
 
-  reload();
   refreshLogin();
 </script>
 
@@ -42,7 +37,7 @@
       }}>Zrušit</Button
     >
     <div class="field-image-container">
-      {#each imageList as image}
+      {#each imageList.slice(perPage * (page - 1), perPage * page) as image}
         <div class="thumbnail-container">
           <img
             class="thumbnail-image"
@@ -57,102 +52,10 @@
           />
         </div>
       {/each}
-      {#if imageList.length > perPage}
-        <div id="pagination">
-          {#if page > 3}
-            <div
-              class="pagination-button"
-              on:click={() => {
-                page = 1;
-                reload();
-              }}
-              on:keypress={() => {
-                page = 1;
-                reload();
-              }}
-            >
-              1
-            </div>
-            ...
-          {/if}
-          {#if page > 2}
-            <div
-              class="pagination-button"
-              on:click={() => {
-                page = page - 2;
-                reload();
-              }}
-              on:keypress={() => {
-                page = page - 2;
-                reload();
-              }}
-            >
-              {page - 2}
-            </div>
-          {/if}
-          {#if page > 1}
-            <div
-              class="pagination-button"
-              on:click={() => {
-                page = page - 1;
-                reload();
-              }}
-              on:keypress={() => {
-                page = page - 1;
-                reload();
-              }}
-            >
-              {page - 1}
-            </div>
-          {/if}
-          <div class="pagination-button active">{page}</div>
-          {#if page < Math.ceil(imageList.length / perPage)}
-            <div
-              class="pagination-button"
-              on:click={() => {
-                page = page + 1;
-                reload();
-              }}
-              on:keypress={() => {
-                page = page + 1;
-                reload();
-              }}
-            >
-              {page + 1}
-            </div>
-          {/if}
-          {#if page < Math.ceil(imageList.length / perPage) - 1}
-            <div
-              class="pagination-button"
-              on:click={() => {
-                page = page + 2;
-                reload();
-              }}
-              on:keypress={() => {
-                page = page + 2;
-                reload();
-              }}
-            >
-              {page + 2}
-            </div>
-          {/if}
-          {#if page < Math.ceil(imageList.length / perPage) - 2}
-            ... <div
-              class="pagination-button"
-              on:click={() => {
-                page = Math.ceil(imageList.length / perPage);
-                reload();
-              }}
-              on:keypress={() => {
-                page = Math.ceil(imageList.length / perPage);
-                reload();
-              }}
-            >
-              {Math.ceil(imageList.length / perPage)}
-            </div>
-          {/if}
-        </div>
-      {/if}
+      <Pagination
+        total={Math.ceil(imageList.length / perPage)}
+        bind:current={page}
+      />
     </div>
   {/await}
 </DoubleSidePanel>
