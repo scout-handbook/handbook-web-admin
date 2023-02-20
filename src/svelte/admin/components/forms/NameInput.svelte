bbe15a676a4e21ecdf108473227fea3626f27c3b
Marek Dědič <developer@dedic.eu>
2023-02-20 14:01:52 +0100

    Moved NameInput to forms directory

---
+++ src/svelte/admin/components/forms/NameInput.svelte
@@ -0,0 +1,12 @@
+<script lang="ts" strictEvents>
+  export let value: string;
+</script>
+
+<input class="form-text name-field" autocomplete="off" type="text" bind:value />
+
+<style>
+  .name-field {
+    font-size: 1.5em;
+    font-weight: bold;
+  }
+</style>
