40bdf02e379e58eaf096b0ddd3260a7e82c7b6aa
Marek Dědič <developer@dedic.eu>
2023-02-20 14:18:43 +0100

    Removed some of competence view CSS

---
+++ src/svelte/admin/components/forms/NumberNameInput.svelte
@@ -0,0 +1,12 @@
+<script lang="ts" strictEvents>
+  export let value: number;
+</script>
+
+<input class="form-text form-name" autocomplete="off" type="text" bind:value />
+
+<style>
+  input {
+    margin-left: 5px;
+    width: 50px;
+  }
+</style>
