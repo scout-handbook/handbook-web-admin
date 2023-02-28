7f542d7b32a25d7b9b7b5f68699ddf580107fd7e
Marek Dědič <developer@dedic.eu>
2023-02-28 22:43:59 +0100

    Split out Select component

---
+++ src/svelte/admin/components/forms/Select.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" strictEvents>
+  // eslint-disable-next-line no-undef
+  type KeyType = $$Generic<number | string>;
+  // eslint-disable-next-line no-undef
+  type ValueType = $$Generic;
+
+  export let options: Array<[KeyType, ValueType]>;
+  export let selected: KeyType | null;
+</script>
+
+<select class="form-select filter-select" bind:value={selected}>
+  {#each options as [id, value]}
+    <option value={id}>{value}</option>
+  {/each}
+</select>
+
+<style>
+  .filter-select {
+    margin-right: 10px;
+  }
+</style>
