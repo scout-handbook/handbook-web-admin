c201dbcfc2086256ec65040be402382319f2bde6
Marek Dědič <developer@dedic.eu>
2023-02-28 22:51:46 +0100

    Moved select styles into the component

--- src/svelte/admin/components/forms/Select.svelte
+++ src/svelte/admin/components/forms/Select.svelte
@@ -8,14 +8,26 @@
   export let selected: KeyType | null;
 </script>
 
-<select class="form-select filter-select" bind:value={selected}>
+<select bind:value={selected}>
   {#each options as [id, value]}
     <option value={id}>{value}</option>
   {/each}
 </select>
 
 <style>
-  .filter-select {
+  select {
     margin-right: 10px;
+    background-color: var(--background-darker);
+    border: 1px solid var(--border-color);
+    border-radius: 3px;
+    font-family: "Open Sans", sans-serif;
+    font-size: 1em;
+    padding: 2px 10px;
+    transition: border-color ease 0.15s, background-color ease 0.15s;
+  }
+
+  select:focus {
+    background-color: var(--background-darkest);
+    border-color: var(--accent-color);
   }
 </style>
