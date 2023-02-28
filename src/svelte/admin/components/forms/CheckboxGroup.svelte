7f76fd698c7c75d98150559d712ee8a052b5e41c
Marek Dědič <developer@dedic.eu>
2023-02-28 15:43:20 +0100

    Moved checkbox styles to the component

--- src/svelte/admin/components/forms/CheckboxGroup.svelte
+++ src/svelte/admin/components/forms/CheckboxGroup.svelte
@@ -14,8 +14,34 @@
   <div class="form-row">
     <label class="form-switch">
       <input type="checkbox" value={id} bind:group={selected} />
-      <span class="form-custom form-checkbox" />
+      <span class="form-custom" />
       <slot {id} {value} />
     </label>
   </div>
 {/each}
+
+<style>
+  span {
+    border-radius: 3px;
+  }
+
+  span::before {
+    border: 3px solid var(--border-color);
+    border-right: none;
+    border-top: none;
+    height: 4px;
+    margin-left: 2px;
+    margin-top: 3px;
+    transform: rotate(-45deg);
+    transition: border-color ease 0.15s;
+    width: 8px;
+  }
+
+  span:hover::before {
+    border-color: var(--accent-color);
+  }
+
+  input:checked + span::before {
+    border-color: var(--accent-color);
+  }
+</style>
