fd0d04b3fb3329507fc3c8e1793b99a3703db918
Marek Dědič <developer@dedic.eu>
2023-03-01 10:54:42 +0100

    Fixed NumberNameInput styles

--- src/svelte/admin/components/forms/NumberNameInput.svelte
+++ src/svelte/admin/components/forms/NumberNameInput.svelte
@@ -2,15 +2,17 @@
   export let value: number;
 </script>
 
-<input class="form-name" autocomplete="off" type="text" bind:value />
+<input autocomplete="off" type="text" bind:value />
 
 <style>
   input {
     border: 1px solid var(--border-color);
     border-radius: 3px;
     font-family: "Open Sans", sans-serif;
+    font-size: 1.5em;
+    font-weight: bold;
     margin-left: 5px;
-    padding: 5px 10px;
+    padding: 10px 16px;
     transition: border-color ease 0.15s, background-color ease 0.15s;
     width: 50px;
   }
