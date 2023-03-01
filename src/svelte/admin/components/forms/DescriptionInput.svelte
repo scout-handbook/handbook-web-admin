2821b6f1b3d2ffaab493a5b7f3414002c17c0e88
Marek Dědič <developer@dedic.eu>
2023-03-01 10:50:10 +0100

    Moved form-text calss into components

--- src/svelte/admin/components/forms/DescriptionInput.svelte
+++ src/svelte/admin/components/forms/DescriptionInput.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <textarea
-  class="form-text description"
   autocomplete="off"
   rows="5"
   bind:value
@@ -11,6 +10,18 @@
 
 <style>
   textarea {
+    border: 1px solid var(--border-color);
+    border-radius: 3px;
+    font-family: "Open Sans", sans-serif;
     margin-top: 20px;
+    padding: 5px 10px;
+    transition: border-color ease 0.15s, background-color ease 0.15s;
+    width: 90%;
+  }
+
+  textarea:focus {
+    background-color: var(--background-darkest);
+    border-color: var(--accent-color);
+    outline: none;
   }
 </style>
