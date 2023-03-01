77599bdd00caf16cccfd467835770e2adfc6953f
Marek Dědič <developer@dedic.eu>
2023-03-01 10:50:10 +0100

    Moved form-text calss into components

--- src/svelte/admin/components/forms/NumberNameInput.svelte
+++ src/svelte/admin/components/forms/NumberNameInput.svelte
@@ -2,11 +2,22 @@
   export let value: number;
 </script>
 
-<input class="form-text form-name" autocomplete="off" type="text" bind:value />
+<input class="form-name" autocomplete="off" type="text" bind:value />
 
 <style>
   input {
+    border: 1px solid var(--border-color);
+    border-radius: 3px;
+    font-family: "Open Sans", sans-serif;
     margin-left: 5px;
+    padding: 5px 10px;
+    transition: border-color ease 0.15s, background-color ease 0.15s;
     width: 50px;
+  }
+
+  input:focus {
+    background-color: var(--background-darkest);
+    border-color: var(--accent-color);
+    outline: none;
   }
 </style>
