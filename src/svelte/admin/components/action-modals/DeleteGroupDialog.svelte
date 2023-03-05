4f1d812b3b360c786943b923f1b66369f7618837
Marek Dědič <developer@dedic.eu>
2023-03-05 19:13:36 +0100

    Added sswr mutation to group deleting

--- src/svelte/admin/components/action-modals/DeleteGroupDialog.svelte
+++ src/svelte/admin/components/action-modals/DeleteGroupDialog.svelte
@@ -1,11 +1,14 @@
 <script lang="ts" strictEvents>
+  import { mutate } from "sswr";
   import { useNavigate } from "svelte-navigator";
 
   import type { Group } from "../../../../ts/admin/interfaces/Group";
+  import type { SWRMutateFix } from "../../../../ts/admin/interfaces/SWRMutateFix";
   import { apiUri } from "../../../../ts/admin/stores";
   import { Action } from "../../../../ts/admin/tools/Action";
   import { ActionQueue } from "../../../../ts/admin/tools/ActionQueue";
   import { get } from "../../../../ts/admin/tools/arrayTools";
+  import { constructURL } from "../../../../ts/admin/tools/constructURL";
   import Dialog from "../Dialog.svelte";
   import DoneDialog from "../DoneDialog.svelte";
 
@@ -19,12 +22,21 @@
 
   function confirmCallback(): void {
     donePromise = new ActionQueue([
-      // TODO: SSWR revalidation/mutation
       new Action(
         $apiUri + "/v1.0/group/" + encodeURIComponent(payload.groupId),
         "DELETE"
       ),
-    ]).dispatch();
+    ])
+      .dispatch()
+      .then(() => {
+        mutate<SWRMutateFix<Record<string, Group>>>(
+          constructURL("v1.0/group"),
+          (groups) => {
+            delete groups[payload.groupId];
+            return groups;
+          }
+        );
+      });
   }
 </script>
 
