4589317aaa3e80c41d664726b41cdf4c86505f1e
Marek Dědič <developer@dedic.eu>
2023-03-06 09:54:22 +0100

    Added sswr mutation to fields deleting

--- src/svelte/admin/components/action-modals/DeleteFieldDialog.svelte
+++ src/svelte/admin/components/action-modals/DeleteFieldDialog.svelte
@@ -1,11 +1,14 @@
 <script lang="ts" strictEvents>
+  import { mutate } from "sswr";
   import { useNavigate } from "svelte-navigator";
 
   import type { Field } from "../../../../ts/admin/interfaces/Field";
+  import type { SWRMutateFix } from "../../../../ts/admin/interfaces/SWRMutateFix";
   import { apiUri } from "../../../../ts/admin/stores";
   import { Action } from "../../../../ts/admin/tools/Action";
   import { ActionQueue } from "../../../../ts/admin/tools/ActionQueue";
   import { get } from "../../../../ts/admin/tools/arrayTools";
+  import { constructURL } from "../../../../ts/admin/tools/constructURL";
   import Dialog from "../Dialog.svelte";
   import DoneDialog from "../DoneDialog.svelte";
 
@@ -19,12 +22,18 @@
 
   function confirmCallback(): void {
     donePromise = new ActionQueue([
-      // TODO: SSWR revalidation/mutation
       new Action(
         $apiUri + "/v1.0/field/" + encodeURIComponent(payload.fieldId),
         "DELETE"
       ),
     ]).dispatch();
+    mutate<SWRMutateFix<Record<string, Field>>>(
+      constructURL("v1.0/field?override-group=true"),
+      (fields) => {
+        delete fields[payload.fieldId];
+        return fields;
+      }
+    );
   }
 </script>
 
