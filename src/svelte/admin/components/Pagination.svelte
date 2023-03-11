44781ac51881a3ef2a5c8eef5c5f8d6bc50c4704
Marek Dědič <developer@dedic.eu>
2023-03-11 23:00:39 +0100

    Split out PaginationButton

--- src/svelte/admin/components/Pagination.svelte
+++ src/svelte/admin/components/Pagination.svelte
@@ -1,4 +1,6 @@
 <script lang="ts" strictEvents>
+  import PaginationButton from "./PaginationButton.svelte";
+
   export let current: number;
   export let total: number;
 </script>
@@ -6,88 +8,49 @@
 {#if total > 1}
   <div class="container">
     {#if current > 3}
-      <!-- TODO: Simplify by splitting out the button component -->
-      <div
-        class="button"
+      <PaginationButton
         on:click={() => {
           current = 1;
-        }}
-        on:keypress={() => {
-          current = 1;
-        }}
+        }}>1</PaginationButton
       >
-        1
-      </div>
       ...
     {/if}
     {#if current > 2}
-      <div
-        class="button"
+      <PaginationButton
         on:click={() => {
           current -= 2;
-        }}
-        on:keypress={() => {
-          current -= 2;
-        }}
+        }}>{current - 2}</PaginationButton
       >
-        {current - 2}
-      </div>
     {/if}
     {#if current > 1}
-      <div
-        class="button"
+      <PaginationButton
         on:click={() => {
           current -= 1;
-        }}
-        on:keypress={() => {
-          current -= 1;
-        }}
+        }}>{current - 1}</PaginationButton
       >
-        {current - 1}
-      </div>
     {/if}
-    <div class="button active">
-      {current}
-    </div>
+    <PaginationButton active>{current}</PaginationButton>
     {#if current < total}
-      <div
-        class="button"
+      <PaginationButton
         on:click={() => {
           current += 1;
-        }}
-        on:keypress={() => {
-          current += 1;
-        }}
+        }}>{current + 1}</PaginationButton
       >
-        {current + 1}
-      </div>
     {/if}
     {#if current < total - 1}
-      <div
-        class="button"
+      <PaginationButton
         on:click={() => {
           current += 2;
-        }}
-        on:keypress={() => {
-          current += 2;
-        }}
+        }}>{current + 2}</PaginationButton
       >
-        {current + 2}
-      </div>
     {/if}
     {#if current < total - 2}
       ...
-      <div
-        class="button"
+      <PaginationButton
         on:click={() => {
           current = total;
-        }}
-        on:keypress={() => {
-          current = total;
-        }}
+        }}>{total}</PaginationButton
       >
-        {total}
-      </div>
     {/if}
   </div>
 {/if}
@@ -96,24 +59,5 @@
   .container {
     margin-top: 40px;
     text-align: center;
-  }
-
-  .button {
-    background-color: var(--background-darker);
-    border: 1px solid var(--border-color);
-    border-radius: 3px;
-    cursor: pointer;
-    display: inline-block;
-    margin: 0 8px;
-    padding: 3px 5px;
-    transition: color ease 0.15s, border-color ease 0.15s,
-      background-color ease 0.15s;
-  }
-
-  .button:hover,
-  .active {
-    background-color: var(--background-darkest);
-    border-color: var(--accent-color);
-    color: var(--accent-color);
   }
 </style>
