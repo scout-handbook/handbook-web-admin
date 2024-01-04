a846a9c1f6ef2eeae5c93475673717125b6150b3
Marek Dědič <developer@dedic.eu>
2024-01-04 20:24:50 +0100

    Enabled chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { readFileSync } from "fs";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite";
 
 function getConfig(mode: string): unknown {
   const location = loadEnv(mode, process.cwd()).VITE_CONFIG;
@@ -11,16 +11,17 @@ function getConfig(mode: string): unknown {
 }
 
 export default defineConfig(({ mode }) => ({
+  plugins: [
+    splitVendorChunkPlugin(),
+    svelte({
+      configFile: "../svelte.config.js",
+    }),
+  ],
   root: "src",
   base: "./",
   build: {
     outDir: "../dist",
   },
-  plugins: [
-    svelte({
-      configFile: "../svelte.config.js",
-    }),
-  ],
   define: {
     CONFIG: getConfig(mode),
   },
