052c9fd303a2784f889b493d2558d0375002170e
Marek Dědič <developer@dedic.eu>
2024-01-05 12:23:39 +0100

    Manually added CSP hashes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import legacy from "@vitejs/plugin-legacy";
+import legacy, { cspHashes } from "@vitejs/plugin-legacy";
 import { readFileSync } from "fs";
 import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite";
+
+if (
+  JSON.stringify(cspHashes) !==
+  JSON.stringify([
+    "MS6/3FCg4WjP9gwgaBGwLpRCY6fZBgwmhVCdrPrNf3E=",
+    "tQjf8gvb2ROOMapIxFvFAYBeUJ0v1HCbOcSmDNXGtDo=",
+    "4y/gEB2/KIwZFTfNqwXJq4olzvmQ0S214m9jwKgNXoc=",
+    "+5XkZFazzJo8n0iOP4ti/cLCMUudTf//Mzkb7xNPXIc=",
+  ])
+) {
+  throw new Error(
+    "CSP Hashes are out of date in htaccess. New hashes: " +
+      JSON.stringify(cspHashes),
+  );
+}
 
 function getConfig(mode: string): unknown {
   const location = loadEnv(mode, process.cwd()).VITE_CONFIG;
