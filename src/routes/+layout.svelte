b2db5ab05079225d682be3dc826855b8e1e552c8
Marek Dědič <developer@dedic.eu>
2024-01-22 21:33:48 +0100

    Added global layout features

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,29 @@
 <script lang="ts" strictEvents>
-  import { siteName } from "$lib/stores";
+  import Dialog from "$lib/components/Dialog.svelte";
+  import LoadingIndicator from "$lib/components/LoadingIndicator.svelte";
+  import Overlay from "$lib/components/Overlay.svelte";
+  import {
+    globalDialogMessage,
+    globalLoadingIndicator,
+    siteName,
+  } from "$lib/stores";
 </script>
 
 <svelte:head>
   <title>{$siteName}</title>
 </svelte:head>
+
+{#if $globalLoadingIndicator}
+  <Overlay />
+  <LoadingIndicator darkBackground />
+{/if}
+{#if $globalDialogMessage !== null}
+  <Dialog
+    confirmButtonText="OK"
+    on:confirm={() => {
+      globalDialogMessage.set(null);
+    }}
+  >
+    {$globalDialogMessage}
+  </Dialog>
+{/if}
