aa6ab7bf46627265706e05699c97a32460379cd8
Marek Dědič <developer@dedic.eu>
2024-01-24 11:47:08 +0100

    Split out main page container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,10 @@
 <script lang="ts" strictEvents>
+  import MainPageContainer from "$lib/components/MainPageContainer.svelte";
   import TopBar from "$lib/components/TopBar.svelte";
   import LessonView from "$lib/views/LessonView.svelte";
 </script>
 
 <TopBar />
-<div class="main-page-container">
-  <div class="main-page">
-    <LessonView />
-  </div>
-</div>
-
-<style>
-  .main-page {
-    margin: 0 auto;
-    max-width: 770px;
-    padding: 20px;
-  }
-
-  .main-page-container {
-    bottom: 0;
-    left: 0;
-    overflow-y: auto;
-    position: absolute;
-    right: 0;
-    top: 81px;
-  }
-</style>
+<MainPageContainer>
+  <LessonView />
+</MainPageContainer>
