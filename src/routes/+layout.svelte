6e4fe115969720645fc9a74164071692af36a8cd
Marek Dědič <developer@dedic.eu>
2024-01-24 11:40:40 +0100

    Fixed global styles & fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,10 @@
 <script lang="ts" strictEvents>
+  import "$lib/font/fontello.css";
+  import "@fontsource/open-sans/400.css";
+  import "@fontsource/open-sans/400-italic.css";
+  import "@fontsource/open-sans/700.css";
+  import "@fontsource/open-sans/700-italic.css";
+
   import Dialog from "$lib/components/Dialog.svelte";
   import LoadingIndicator from "$lib/components/LoadingIndicator.svelte";
   import Overlay from "$lib/components/Overlay.svelte";
@@ -36,3 +42,22 @@
 {/if}
 
 <slot />
+
+<style>
+  :global(body) {
+    color: var(--background);
+    font-family: "Open Sans", sans-serif;
+    font-feature-settings: "liga" 1; /* stylelint-disable-line plugin/no-unsupported-browser-features */
+    font-kerning: normal; /* stylelint-disable-line plugin/no-unsupported-browser-features */
+    font-size: 16px;
+    height: 100%;
+    line-height: 160%;
+    margin: 0;
+    position: absolute;
+    width: 100%;
+  }
+
+  :global(h1) {
+    line-height: 140%;
+  }
+</style>
