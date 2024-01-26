2cae1cf7c6a04890b880d570fed7dcfe049e1ef5
Marek Dědič <developer@dedic.eu>
2024-01-26 19:59:38 +0100

    [WIP] Deleted obsolete files

--- src/ts/admin/utils/getQueryField.ts
+++
@@ -1,7 +0,0 @@
-export function getQueryField(search: string, key: string): string | null {
-  const keyValuePair = new RegExp(`[?&]${key}(=([^&#]*)|&|#|$)`).exec(search);
-  if (keyValuePair === null || keyValuePair.length < 2) {
-    return null;
-  }
-  return decodeURIComponent(keyValuePair[2].replace(/\+/g, " "));
-}
