5df2966224e7ffdc5b48e191351a876b04db3f6f
Marek Dědič <developer@dedic.eu>
2024-01-26 19:52:19 +0100

    Moved lesson edit page to SvelteKit

---
+++ src/routes/lessons/[id]/edit/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts" strictEvents>
+  import FieldProvider from "$lib/components/swr-wrappers/FieldProvider.svelte";
+  import EditLessonPage from "$lib/EditLessonPage.svelte";
+
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<FieldProvider let:fields let:lessons>
+  <EditLessonPage {fields} lessonID={data.id} {lessons} />
+</FieldProvider>
