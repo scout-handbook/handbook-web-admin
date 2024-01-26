2bd4f4fbaba902f8468b34927225ba76ac5907cf
Marek Dědič <developer@dedic.eu>
2024-01-26 19:59:03 +0100

    Lesson restoring using sveltekit

---
+++ src/routes/lessons/[id]/versions/[version]/restore/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts" strictEvents>
+  import RestoreLessonView from "$lib/views/RestoreLessonView.svelte";
+
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<RestoreLessonView lessonID={data.id} version={data.version} />
