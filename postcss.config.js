906cfb889a6fb38c1a680e8d83918dd9d0562555
Marek Dědič <developer@dedic.eu>
2024-01-27 19:30:19 +0100

    Fixed passing of theme file to PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,11 @@
 import postcssGlobalData from "@csstools/postcss-global-data";
 import autoprefixer from "autoprefixer";
 import postcssCustomProperties from "postcss-custom-properties";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
 
 function getThemeFiles() {
   const themeFiles = ["src/lib/default-theme.css"];
-  const argTheme = yargs(hideBin(process.argv)).string("theme").argv.theme;
-  if (argTheme !== undefined) {
+  const argTheme = process.env.THEME;
+  if (argTheme !== undefined && argTheme !== "undefined" && argTheme !== "") {
     themeFiles.push(argTheme);
   }
   return themeFiles;
