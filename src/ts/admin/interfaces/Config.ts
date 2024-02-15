b2dca0869029b17bd00854ed099fb4e1eae2fd45
Marek Dědič <developer@dedic.eu>
2024-02-15 11:56:05 +0100

    Fixed naming convention

--- src/ts/admin/interfaces/Config.ts
+++ src/ts/admin/interfaces/Config.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/naming-convention -- External configuration */
 export interface Config {
   "api-uri": string;
   "admin-uri": string;
   "frontend-uri": string;
   "site-name": string;
 }
+/* eslint-enable */
