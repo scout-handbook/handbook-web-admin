a75e99e5b2b54fc74f37f4fcbb105978fd60f4c1
Marek Dědič <developer@dedic.eu>
2024-10-17 18:48:22 +0200

    More precise API response types

--- src/ts/admin/interfaces/APIResponse.ts
+++ src/ts/admin/interfaces/APIResponse.ts
@@ -1,9 +1,26 @@
 import type { RequestResponse } from "./RequestResponse";
 
-export interface APIResponse<T extends RequestResponse> {
-  holder?: string;
-  message?: string;
-  response?: T;
-  status: number;
-  type?: string;
+export interface APISuccessResponse<T extends RequestResponse> {
+  response: T;
+  status: 200 | 201;
 }
+
+export type APIErrorResponse =
+  | {
+      holder: string;
+      message: string;
+      status: 409;
+      type: string;
+    }
+  | {
+      message: string;
+      status: 400 | 403 | 404 | 412 | 415 | 500;
+      type: string;
+    }
+  | {
+      status: 401;
+    };
+
+export type APIResponse<T extends RequestResponse> =
+  | APIErrorResponse
+  | APISuccessResponse<T>;
