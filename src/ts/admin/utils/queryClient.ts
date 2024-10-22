314278594aa742913e1a6f532b324930afbb6486
Marek Dědič <developer@dedic.eu>
2024-10-22 18:57:32 +0200

    Deduplicated query building

--- src/ts/admin/utils/queryClient.ts
+++ src/ts/admin/utils/queryClient.ts
@@ -5,25 +5,8 @@ import type { Payload } from "../interfaces/Payload";
 import type { RequestResponse } from "../interfaces/RequestResponse";
 
 import { apiUri } from "../stores";
+import { buildQuery } from "./buildQuery";
 import { reAuth, request } from "./request";
-
-function constructQuery(searchParams: Payload): string {
-  const pairs: Array<string> = [];
-  for (const key in searchParams) {
-    if (!Object.prototype.hasOwnProperty.call(searchParams, key)) {
-      continue;
-    }
-    const value = searchParams[key];
-    if (Array.isArray(value)) {
-      for (const instance of value) {
-        pairs.push(`${key}[]=${instance}`);
-      }
-    } else if (value !== undefined) {
-      pairs.push(`${key}=${value.toString()}`);
-    }
-  }
-  return pairs.join("&");
-}
 
 async function queryFn({
   queryKey,
@@ -31,7 +14,7 @@ async function queryFn({
   queryKey: ReadonlyArray<unknown>;
 }): Promise<RequestResponse> {
   let uri = queryKey.filter((elem) => typeof elem === "string").join("/");
-  const query = constructQuery(
+  const query = buildQuery(
     (queryKey.find((elem) => typeof elem === "object") as
       | Payload
       | undefined) ?? {},
