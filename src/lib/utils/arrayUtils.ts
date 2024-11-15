8c0bd797a910fddd2cf1ca14069acc5b8c52bd28
Marek Dědič <developer@dedic.eu>
2024-11-15 20:51:55 +0100

    Removed obsolete code

--- src/lib/utils/arrayUtils.ts
+++ src/lib/utils/arrayUtils.ts
@@ -5,20 +5,9 @@ export function filter<T>(
   return arr.filter(([id, _]) => fn(id));
 }
 
-export function get<T>(arr: Array<[string, T]>, id: string): T | undefined {
-  return arr.find(([elementId, _]) => elementId === id)?.[1];
-}
-
 export function map<T, U>(
   arr: Array<[string, T]>,
   fn: (item: T) => U,
 ): Array<[string, U]> {
   return arr.map(([id, value]) => [id, fn(value)]);
-}
-
-export function sort<T>(
-  arr: Array<[string, T]>,
-  comparator: (first: T, second: T) => number,
-): Array<[string, T]> {
-  return arr.sort(([_1, first], [_2, second]) => comparator(first, second));
 }
