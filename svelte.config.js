8dbb2cffd7271abb2ae1a1b5cef0b5924b7723d9
Marek Dědič <developer@dedic.eu>
2025-06-29 00:27:20 +0200

    Reading basepath from env directly

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,5 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { readFileSync } from "fs";
-
-function getConfig() {
-  if (!(process.env["npm_lifecycle_script"]?.startsWith("vite") ?? false)) {
-    return {};
-  }
-  const location = process.env.VITE_CONFIG;
-  if (location === undefined || location === "undefined") {
-    throw new Error("No config specified");
-  }
-  return JSON.parse(readFileSync(location, "utf8"));
-}
-
-const config = getConfig();
-const basePath =
-  config["admin-uri"] !== undefined
-    ? new URL(config["admin-uri"]).pathname
-    : "";
 
 export default {
   kit: {
@@ -40,7 +22,7 @@ export default {
       mode: "hash",
     },
     paths: {
-      base: basePath,
+      base: `/${process.env.VITE_BASEPATH ?? "admin"}`,
     },
   },
   preprocess: vitePreprocess(),
