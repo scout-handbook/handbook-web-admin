bb568795148c1030dc9fa83b4a4c2acaff351489
Marek Dědič <developer@dedic.eu>
2025-06-29 10:31:47 +0200

    Removed custom theming ability

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,9 @@ import postcssGlobalData from "@csstools/postcss-global-data";
 import autoprefixer from "autoprefixer";
 import postcssCustomProperties from "postcss-custom-properties";
 
-function getThemeFiles() {
-  const themeFiles = ["src/lib/default-theme.css"];
-  const argTheme = process.env.VITE_THEME;
-  if (argTheme !== undefined && argTheme !== "undefined") {
-    themeFiles.push(argTheme);
-  }
-  return themeFiles;
-}
-
 export default {
   plugins: [
-    postcssGlobalData({ files: getThemeFiles() }),
+    postcssGlobalData({ files: ["src/lib/default-theme.css"] }),
     postcssCustomProperties({ preserve: false }),
     autoprefixer(),
   ],
